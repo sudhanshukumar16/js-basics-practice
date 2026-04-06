4635566b02970c42f098698de860ce3d00802628
Sudhanshu Kumar <idealhonestboy@gmail.com>
2026-04-06 13:01:28 +0530

    created a bot

--- consolelog.js
+++ consolelog.js
@@ -29,3 +29,59 @@ const favoriteSubjectSentence = "My favorite subject is " + favoriteSubject + ".
 console.log(favoriteSubjectSentence);
 
 console.log("Well, it was nice to talk to you. Have a nice day!");
+
+
+
+
+
+//6apr
+
+
+// console.log("Hello! I'm your coding fun fact guide!");
+// let botName ="Bot";
+// let botLocation = "Rohtak";
+// let favoriteLanguage= "cpp";
+// console.log("My name is "+botName+ " and I live on "+botLocation+".");
+
+// console.log("My favorite programming language is "+favoriteLanguage +".");
+
+// let codingFact = "It is widely used for system programming like " + favoriteLanguage + ".";
+
+// console.log(codingFact);
+// favoriteLanguage= "c"
+
+
+
+// codingFact = "It is powerful and fast like " + favoriteLanguage + ".";
+
+// console.log(codingFact);
+
+// favoriteLanguage= "python";
+// codingFact = favoriteLanguage + " is easy to learn and widely used.";
+
+// console.log(codingFact);
+
+// console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
+
+
+// correct one
+
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Bot";
+let botLocation = "Rohtak";
+let favoriteLanguage = "cpp";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = favoriteLanguage + " is widely used for system programming.";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " is known for high performance.";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " is used in competitive programming.";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
